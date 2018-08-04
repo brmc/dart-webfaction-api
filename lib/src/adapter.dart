adb76548aa8b2c4f4351490bfb7c21c14428bd42
Brian <brian.mcclr@gmail.com>
2018-08-04 09:42:53 +0200

    Introduced RPC adapter instead of passing function

---
+++ lib/src/adapter.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'package:xml_rpc/client.dart' as xml_rpc;
+
+abstract class RpcAdapter {
+  Future call(String methodName, List params);
+}
+
+const webfactionUrl = 'https://api.webfaction.com/';
+
+class WebfactionRpc implements RpcAdapter{
+  @override
+  Future call(String methodName, List params) {
+    return xml_rpc.call(webfactionUrl, methodName, params);
+  }
+}
